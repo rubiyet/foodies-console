346a912cae8a63f623f57962154fb64d259d86a8
rubiyet <rubiyetfardous@gmail.com>
2022-12-30 19:27:02 +0600

    available quantity check

--- components/Products.tsx
+++ components/Products.tsx
@@ -7,7 +7,21 @@ import { getProducts } from "../lib/helper";
 import Image from "next/image";
 
 export default function UserCard() {
+  const [itemAdded, setItemAdded] = useState(false);
   const dispatch = useDispatch();
+  const [cart, setCart] = useState<any>([]);
+  console.log("cart", cart);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const cs = localStorage.getItem("cart");
+      console.log("cs", cs);
+      if (cs) {
+        const cart1 = JSON.parse(cs);
+        setCart(cart1);
+      }
+    }
+  }, [itemAdded, cart]);
 
   const { data, error, isError, isLoading } = useQuery(
     "getProducts",
@@ -26,11 +40,13 @@ export default function UserCard() {
   const data1 = useSelector(getCartState);
 
   const onClick = (item: number) => {
+    setItemAdded(true);
     const cs = localStorage.getItem("cart");
+    console.log("cs", cs);
     let isAdded = false;
     if (cs) {
-      const cart = JSON.parse(cs);
-      console.log("cart", cart);
+      // const cart = JSON.parse(cs);
+      // console.log("cart", cart);
       cart.map((i: any) => {
         if (i.item === item) {
           isAdded = true;
@@ -58,44 +74,87 @@ export default function UserCard() {
     <>
       <div className="grid grid-cols-4 gap-y-10 gap-x-10">
         {data &&
-          data.map((item: any) => (
-            <div key={item.id} className="w-44 xl:w-44 2xl:w-56 rounded-b-lg">
-              <Image
-                src={item.image}
-                alt="image"
-                width={860}
-                height={850}
-                className="h-48 xl:h-48 2xl:w-64 rounded-t-lg"
-              />
-              <div className="bg-white h-20 px-4 py-3 rounded-b-lg space-y-2">
-                <div className="flex justify-between items-centers">
-                  {item.name}
-                  <span>৳{item.price}</span>
-                </div>
-                <div className="flex items-center justify-between space-x-2">
-                  <div className="2xl:flex items-center 2xl:space-x-2 space-y-1 2xl:space-y-0">
-                    <div className="flex items-center space-x-1 bg-gray-50 border border-gray-200 rounded-lg px-2 py-[0.1rem] xl:text-xs">
-                      <Image src="/star.svg" alt="veg" width={14} height={15} />
-                      <span>4.7</span>
-                    </div>
-                    <div className="flex items-center space-x-1 bg-gray-50 border border-gray-200 rounded-lg px-2 py-[0.1rem] xl:text-xs">
-                      50-79 min
-                    </div>
+          data.map((item: any) => {
+            let isAdded = false;
+            let quantityCount = 0;
+            let quantityExist = true;
+            {
+              cart?.map((i: any) => {
+                isAdded = true;
+                if (i.item === item.id) {
+                  if(i.quantity === item.quantity_available) {
+                    quantityExist = false;
+                  }
+                }
+              });
+            }
+            return (
+              <div key={item.id} className="w-44 xl:w-44 2xl:w-56 rounded-b-lg">
+                <Image
+                  src={item.image}
+                  alt="image"
+                  width={860}
+                  height={850}
+                  className="h-48 xl:h-48 2xl:w-64 rounded-t-lg"
+                />
+                <div className="bg-white h-20 px-4 py-3 rounded-b-lg space-y-2">
+                  <div className="flex justify-between items-centers">
+                    {item.name}
+                    <span>৳{item.price}</span>
                   </div>
-                  <div>
-                    <button onClick={() => onClick(item.id)}>
-                      <Image
-                        src="/iconoir_add-to-cart.svg"
-                        alt="veg"
-                        width={20}
-                        height={20}
-                      />
-                    </button>
+                  <div className="flex items-center justify-between space-x-2">
+                    <div className="2xl:flex items-center 2xl:space-x-2 space-y-1 2xl:space-y-0">
+                      <div className="flex items-center space-x-1 bg-gray-50 border border-gray-200 rounded-lg px-2 py-[0.1rem] xl:text-xs">
+                        <Image
+                          src="/star.svg"
+                          alt="veg"
+                          width={14}
+                          height={15}
+                        />
+                        <span>4.7</span>
+                      </div>
+                      <div className="flex items-center space-x-1 bg-gray-50 border border-gray-200 rounded-lg px-2 py-[0.1rem] xl:text-xs">
+                        50-79 min
+                      </div>
+                    </div>
+                    <div>
+                      {isAdded && quantityExist && (
+                        <button onClick={() => onClick(item.id)}>
+                          <Image
+                            src="/iconoir_add-to-cart.svg"
+                            alt="veg"
+                            width={20}
+                            height={20}
+                          />a
+                        </button>
+                      )}
+                      {isAdded && !quantityExist && (
+                        <button onClick={() => onClick(item.id)}>
+                          <Image
+                            src="/outOfStock.svg"
+                            alt="veg"
+                            width={20}
+                            height={20}
+                          />
+                        </button>
+                      )}
+                      {cart.length === 0 && (
+                        <button onClick={() => onClick(item.id)}>
+                          <Image
+                            src="/iconoir_add-to-cart.svg"
+                            alt="veg"
+                            width={20}
+                            height={20}
+                          />
+                          {cart.length}
+                        </button>
+                      )}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
       </div>
     </>
   );
